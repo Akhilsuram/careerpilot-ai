207aaec22547da716a5088a22a816ceff7959218
Suram Akhil <suramakhil999@gmail.com>
2026-07-22 22:11:19 +0530

    Create SQLite database directory automatically

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,12 +1,18 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from backend.core.settings import settings
 
+# Create database directory if it doesn't exist
+db_dir = Path("storage/database")
+db_dir.mkdir(parents=True, exist_ok=True)
+
 engine = create_engine(
     settings.DATABASE_URL,
     echo=False,
-    future=True
+    future=True,
 )
 
 SessionLocal = sessionmaker(
